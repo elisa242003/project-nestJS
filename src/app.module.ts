8b35695247b9f853f16e0c7d58a26307210bf945
elisa242003 <elisarefugio58@gmail.com>
2024-06-09 00:28:23 -0600

    CRUD de estados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StatesModule } from './states/states.module';
 
 @Module({
-  imports: [],
+  imports: [StatesModule],
   controllers: [AppController],
   providers: [AppService],
 })
