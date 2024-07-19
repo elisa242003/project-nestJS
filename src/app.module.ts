200735366976767750bacb674bb829181765789b
elisa242003 <elisarefugio58@gmail.com>
2024-07-19 15:19:20 -0600

    importación de ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,9 +12,11 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     StatesModule,
     CitiesModule,
     TownsModule,
